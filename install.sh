dc60af0c1c86f2c6111cd280c7fb8d38ee71c3be
Michael <Gbps@users.noreply.github.com>
2017-06-20 18:35:46 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
 echo "=============== Gbps Startup Script ==============="
-if [ "$EUID" -ne 0 ]
-then 
-    echo "Please run as root!"
-    echo "sudo $0" 
-    exit
-fi
-
 function execute_cmd
 {
     echo "> $@"
